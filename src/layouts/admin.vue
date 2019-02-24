466ece3091140fe309d2ef1dd196fd6c369270b8
Eduardo P. Rivero <jrperedo@gmail.com>
2019-02-24 10:26:30 -0500

    feat(web): added css for admin layout

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,13 +1,17 @@
 <template>
-  <section>
+  <section class="wrapper">
     <header>
-      <h1>Administrator (only logged users)</h1>
+      <app-text tag-name="h1" class="medium">Administrator</app-text>
     </header>
     <section>
-      <ul>
-        <li>
+      <ul class="list-container">
+        <li class="list-item">
           <nuxt-link to="/admin/dashboard">Dashboard</nuxt-link>
+        </li>
+        <li class="list-item">
           <nuxt-link to="/admin/change-password">Change Password</nuxt-link>
+        </li>
+        <li class="list-item">
           <nuxt-link to="/admin/sign-out">Sign Out</nuxt-link>
         </li>
       </ul>
@@ -17,11 +21,31 @@
 </template>
 
 <script>
+import appText from '@/components/app-text.vue';
+
 export default {
 	name: 'layouts-admin',
+	components: {
+		appText,
+	},
 	middleware: 'identity',
 };
 </script>
 
 <style scoped>
+.list-container {
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	list-style-type: none;
+	padding: var(--largePadding) 0;
+}
+
+.list-item {
+	padding: var(--smallPadding) 0;
+}
+
+.wrapper {
+	padding: var(--largePadding);
+}
 </style>
